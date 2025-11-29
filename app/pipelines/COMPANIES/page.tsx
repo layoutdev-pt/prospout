3201ec054b87eabf1f9f9272640a4fc648ef4e88
Layout Dev <dev@layoutagency.pt>
2025-11-29 16:41:36 +0000

    upgrade

---
+++ app/pipelines/COMPANIES/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+import PipelinePage from '../[pipeline]/page';
+
+export default function CompaniesPipelinePage() {
+  return <PipelinePage params={{ pipeline: 'COMPANIES' }} />;
+}
+
---
+++ app/pipelines/INFLUENCERS/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+import PipelinePage from '../[pipeline]/page';
+
+export default function InfluencersPipelinePage() {
+  return <PipelinePage params={{ pipeline: 'INFLUENCERS' }} />;
+}
+
